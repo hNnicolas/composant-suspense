7a4490ba676fc9bf513965f7406ee74e0ed1f003
huang.nicolas <huang.nicolas3wa.io>
2025-01-27 15:54:49 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>{{ evenement }}</h2>
+  <Suspense
+    @pending="myEvent('PENDING')"
+    @fallback="myEvent('FALLBACK')"
+    @resolve="myEvent('RESOLVE')"
+  >
+    <LazyList />
+    <template #fallback>
+      <h1>Chargement...</h1>
+    </template>
+  </Suspense>
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent, ref } from 'vue'
+
+const evenement = ref('')
+const LazyList = defineAsyncComponent(() => import('./Liste.vue'))
+
+function myEvent(eventName: string) {
+  console.log(eventName)
+  evenement.value = eventName
+}
+</script>
+
+<style lang="scss"></style>
---
+++ src/Liste.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-20">
+    <ul>
+      <li v-for="user of state.users">{{ user.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const state = reactive<{ users: any[] }>({ users: [] })
+
+// Délai de 3secondes grâce à l'option delay de restapi
+state.users = await (await fetch('https://restapi.fr/api/vueusers?delay=3')).json()
+</script>
+
+<style lang="scss"></style>
